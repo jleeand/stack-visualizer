b7e8ca24ffbca947c3f7c21d9b2a6856af248b0c
jleeand <jleeandout@gmail.com>
2022-07-14 17:32:49 -0400

    Add left click logic

--- stack.js
+++ stack.js
@@ -1,11 +1,22 @@
-
 const scence = new THREE.Scene()
+
+
+
+
 
 const geo = new THREE.BoxGeometry(3,1,3)
 const material = new THREE.MeshLambertMaterial({color: 0xC5B4E3})
 const mesh = new THREE.Mesh(geo, material)
 mesh.position.set(0,0,0)
 scence.add(mesh)
+
+// const geo2 = new THREE.BoxGeometry(3,1,3)
+// const material2 = new THREE.MeshLambertMaterial({color: 0xff0000})
+// const mesh2 = new THREE.Mesh(geo, material2)
+// mesh2.position.set(0,1,0)
+// scence.add(mesh2)
+
+
 
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
@@ -31,3 +42,16 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scence, camera);
 
 document.body.appendChild(renderer.domElement);
+
+
+window.addEventListener('click', () => {
+    
+    const geo2 = new THREE.BoxGeometry(3,1,3)
+    const material2 = new THREE.MeshLambertMaterial({color: 0x00ffee})
+    const mesh2 = new THREE.Mesh(geo2, material2)
+    mesh2.position.set(0,1,0)
+    scence.add(mesh2)
+    renderer.render(scence, camera);
+
+})
+
